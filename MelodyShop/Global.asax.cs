ccc98fc080bde37b1ec9c9862558d8735b9f3d45
s.jarzabek <sebastian.jarzabek@outlook.com>
2021-01-10 15:49:21 +0100

    Aktualizacja pakietów nuget, zmiana bundleConfig, dodanie Bootstrap, knockout i entityFramework.

--- MelodyShop/Global.asax.cs
+++ MelodyShop/Global.asax.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -9,7 +5,7 @@ using System.Web.Routing;
 
 namespace MelodyShop
 {
-    public class MvcApplication : System.Web.HttpApplication
+  public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
