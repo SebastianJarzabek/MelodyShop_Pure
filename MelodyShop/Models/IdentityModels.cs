22a5de90a092d1ebbd2f589e18d7776ee7dd5950
s.jarzabek <sebastian.jarzabek@outlook.com>
2021-01-11 23:41:13 +0100

    product

--- MelodyShop/Models/IdentityModels.cs
+++ MelodyShop/Models/IdentityModels.cs
@@ -6,28 +6,32 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MelodyShop.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+  // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+  public class ApplicationUser : IdentityUser
+  {
+    public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
+      // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+      var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+      // Add custom user claims here
+      return userIdentity;
+    }
+  }
+
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+  {
+    public ApplicationDbContext()
+        : base("MelodyShopDB", throwIfV1Schema: false)
+    {
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public static ApplicationDbContext Create()
     {
-        public ApplicationDbContext()
-            : base("MelodyShopDB", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
+      return new ApplicationDbContext();
     }
+
+    public System.Data.Entity.DbSet<MelodyShop.DataAccessLayer.Product> Products { get; set; }
+
+    public System.Data.Entity.DbSet<MelodyShop.DataAccessLayer.Category> Categories { get; set; }
+  }
 }
